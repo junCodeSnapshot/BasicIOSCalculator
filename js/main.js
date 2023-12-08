9e307f2253ec4ecb62e11fe8fa448332d42792ae
JunCode <154179code@gmail.com>
2023-12-08 16:46:37 -0600

    Math and buttons animations working

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ let operatorsBtns = [], numbersBtns = []; //get all numbers and operators
 /*TODO:
 -Add AC function that makes changes on fisrtUseFlag 
 -Exclude the equal button is Active class 
--Mathematical properties fixes
+-isActive can ejecute without modiying the operation array
 */
 
 for (let i = 0; i < buttons.length; i++) {//fill all the arrays with their respective buttons
@@ -28,7 +28,23 @@ for (let i = 0; i < buttons.length; i++) {//fill all the arrays with their respe
 for (let i = 0; i < operatorsBtns.length; i++) {//add the listeners to the buttons
     operatorsBtns[i].addEventListener('click', (e) => {
         activeButtonHandler(e.target);
-        operationDispatcher(e.target.value);
+        if (firstUseFlag) {
+            screenValue = Number(screenValue); 
+            numbers.push(screenValue)
+            firstUseFlag = false;
+        }
+        if(operations[0] && ['plus', 'menos'].includes(e.target.value)){
+            operationDispatcher();
+            cleanUp();
+            valueOnScreenHandler(result);
+            screenFunction();
+        }
+        if(operations[1] && ['division', 'multi'].includes(e.target.value)){
+            operationDispatcher();
+            cleanUp();
+            valueOnScreenHandler(result);
+            screenFunction(); 
+        }
     })
 }
 
@@ -36,9 +52,14 @@ for (let i = 0; i < operatorsBtns.length; i++) {//add the listeners to the butto
 for (let i = 0; i < numbersBtns.length; i++) {//add the listeners to the buttons
     numbersBtns[i].addEventListener('click', (e) => {
 
+        if (!firstUseFlag) {
+            operations.push(pressedButtonOp.value);
+            firstUseFlag = true;
+        }
+
+
         pressedButtonOp ? operationButtonFlag ?
-            (activeButtonHandler(pressedButtonOp), cleanUp(), valueOnScreenHandler(e.target.value), screenFunction()): null : null;
-            (valueOnScreenHandler(e.target.value), screenFunction())
+            (activeButtonHandler(pressedButtonOp), cleanUp(), valueOnScreenHandler(e.target.value), screenFunction()) : (valueOnScreenHandler(e.target.value), screenFunction()) : (valueOnScreenHandler(e.target.value), screenFunction());
     })
 }
 
@@ -70,7 +91,6 @@ const activeButtonHandler = (element) => {
         pressedButtonOp.disabled = operationButtonFlag;
         isActive();
     }
-
 }
 const isActive = () => {
     if (operationButtonFlag) {
@@ -79,73 +99,27 @@ const isActive = () => {
         pressedButtonOp.classList.remove('isActive');
     }
 }
-// const mathOrderHandler = (op) => {
-//     console.log(op)
-//     if(operations[1]  === 'multi' || operations[1] === 'division' && operations[0] === 'menos' || operations[0] === 'plus'){
-//         multiply(numbers[1], numbers[2]);
-//         plus(numbers[0], result);
-//         console.log(result);
-//         screenFunction(result);
-//     }else if(op == 'plus' || op == 'menos'){
 
-//     }
-
-//     console.log('pepe');
-// }
-const operationDispatcher = (op) => {
-    screenValue = Number(screenValue);
-    operations.push(op);
-    numbers.push(screenValue);
-
-    if (operations.length <= 2) {//check the loop
-        if (['plus', 'menos', 'multi', 'division', 'equal'].includes(operations[0]) && ['plus', 'menos', 'equal'].includes(operations[1])) {
-            operations[0] == 'plus' ? plus(numbers[0], numbers[1]) : null;
-            console.log('ayuwoki')
-            operations[0] == 'menos' ? minus(numbers[0], numbers[1]) : null;
-            operations[0] == 'multi' ? multiply(numbers[0], numbers[1]) : null;
-            operations[0] == 'division' ? division(numbers[0], numbers[1]) : null;
-            operations[0] == 'equal' ? equalFunction() : null;
-            cleanUp();//limpia el ultimo numero en screenvalue para que no se supen con el resultdo.
-            valueOnScreenHandler(result); //
-            screenFunction();
-            for (let i = 0; i < operations.length; i++) {
-                operations.shift();
-            }
-            for (let i = 0; i <= numbers.length; i++) {
-                numbers.pop();
-            }
-            numbers.push(result);
-            result = 0;
-        }
+const operationDispatcher = () => {
+    if(['plus', 'menos', 'multi' , 'division'].includes(operations[0])){
+        operations[0] === 'plus' ? plus(numbers[0], numbers[1]): null;
+        operations[0] === 'menos' ? minus(numbers[0], numbers[1]): null;
+        operations[0] === 'multi' ? multiply(numbers[0], numbers[1]): null;
+        operations[0] === 'division' ? division(numbers[0], numbers[1]): null;
+        numbers.push(result);
     }
-    else if (operations.length > 2) {
-        if (['plus', 'menos'].includes(operations[0]) && ['multi', 'division'].includes(operations[1])) {
-            operations[1] == 'multi' ? multiply(numbers[2], numbers[1])
-                : division(numbers[1], numbers[2]);
-
-            operations[0] == 'plus' ? plus(numbers[0], result) : minus(numbers[0], result);
-
-            // for (let i = 0; i < operations.length; i++) {
-            //     operations.shift();
-            // }
-            while (operations.length > 1) {
-                operations.shift();
-            }
-            while (numbers.length > 0) {
-                console.log(numbers.shift());
-            }
-            console.log("Operations");
-
-            numbers.push(result);
-            cleanUp();
-            valueOnScreenHandler(result);
-            screenFunction();
-            result = 0;
-        }
+    if(['multi', 'division'].includes(operations[1])){
+        operations[1] === 'multi' ? multiply(numbers[2], numbers[1]): null;
+        operations[1] === 'division' ? division(numbers[2], numbers[1]): null;
+        operations[0] == 'plus' ? plus(numbers[0], result) : minus(numbers[0], result);
+        numbers.push(result);
     }
-    // cleanUp();
-    // screenFunction();
-    // console.log('not here')
+    while (operations.length > 0) {
+        operations.shift();
+    }
+    while (numbers.length > 1) {
+        console.log(numbers.shift());
+    }
 };
 
 
